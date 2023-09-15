344b240a78621b45feea9c59dc83cb752e8f153d
CondorHero <love2xinwei@gmail.com>
2023-09-15 18:53:33 +0800

    chore: add basic rule

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,7 +2,7 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
 
 // https://github.com/testing-library/jest-dom/issues/515
-// import '@testing-library/jest-dom/vitest';
+// import "@testing-library/jest-dom/vitest";
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 // import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    // viteTsconfigPaths(),
-  ],
-  test: {
-    globals: true,
-    environment: 'happy-dom',
-    setupFiles: ['./src/setupTests.ts'],
-  },
-  server: {
-    port: 3000,
-  },
-  build: {
-    outDir: 'build',
-  },
+	plugins: [
+		react(),
+		// viteTsconfigPaths(),
+	],
+	test: {
+		globals: true,
+		environment: "happy-dom",
+		setupFiles: ["./src/setupTests.ts"],
+	},
+	server: {
+		port: 3000,
+	},
+	build: {
+		outDir: "build",
+	},
 });
