fb0675eafc8a60264741bc3aad88fa0180823c2d
CondorHero <love2xinwei@gmail.com>
2023-09-18 17:23:44 +0800

    chore: add jsx indent

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,20 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>Edit <code>src/App.tsx</code> and save to reload.</p>
+				<p
+				>
+					Edit
+					<code>src/App.tsx</code>
+					and save to reload.
+				</p>
 				<a
 					className="App-link"
 					href="https://reactjs.org"
 					target="_blank"
 					rel="noopener noreferrer"
-				>Learn React</a>
+				>
+					Learn React
+				</a>
 			</header>
 		</div>
 	);
