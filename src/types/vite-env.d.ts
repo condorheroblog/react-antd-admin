9edcecd59ef7292bf847876b3025c86dcb285e5d
CondorHero <love2xinwei@gmail.com>
2023-11-22 14:58:30 +0800

    chore: add import.meta.env intellisense types

--- src/types/vite-env.d.ts
+++ src/types/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+// https://vitejs.dev/guide/env-and-mode.html#intellisense-for-typescript
+interface ImportMetaEnv {
+	readonly VITE_API_BASE_URL: string;
+	readonly VITE_BASE_HOME_PATH: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
