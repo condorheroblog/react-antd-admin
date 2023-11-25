ae65033d48c0f22f678967141b492e2695a3c675
CondorHero <love2xinwei@gmail.com>
2023-11-25 16:54:27 +0800

    fix: change route page flash

--- src/layout/parent-layout/index.tsx
+++ src/layout/parent-layout/index.tsx
@@ -1,5 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { Suspense } from "react";
 
 export default function ParentLayout() {
-	return <Outlet />;
+	return (
+		<Suspense>
+			<Outlet />
+		</Suspense>
+	);
 }
