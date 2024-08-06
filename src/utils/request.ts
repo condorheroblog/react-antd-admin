9ffb4c536d144f306fb0bba5efafc109cacf0c9a
Condor Hero <love2xinwei@gmail.com>
2024-08-06 16:26:01 +0800

    feat: refresh token

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -6,6 +6,9 @@ import { message, rememberRoute } from "#src/utils";
 import { useGlobalStore, useUserStore } from "#src/store";
 import { fetchRefreshToken } from "#src/api/user";
 
+// Requests that do not require authentication.
+const requestWhiteList = ["/login"];
+
 const defaultConfig: Options = {
 	// The input argument cannot start with a slash / when using prefixUrl option.
 	prefixUrl: import.meta.env.VITE_API_BASE_URL,
@@ -24,20 +27,35 @@ const defaultConfig: Options = {
 		afterResponse: [
 			async (request, options, response) => {
 				useGlobalStore.getState().closeGlobalSpin();
+
+				// If the response is not ok(not in the range 200-299), it means that there was an error.
 				if (!response.ok) {
-					if (response.status === 401) {
-						const { refreshToken } = useUserStore.getState();
-						fetchRefreshToken({
-							refreshToken,
-						}).then(async (res) => {
-							useUserStore.setState({
-								token: res.result.token,
-								refreshToken: res.result.refreshToken,
-							});
-						}).catch(() => {
-							// Remember the route before exiting
+					if (!requestWhiteList.some(url => request.url.endsWith(url)) && response.status === 401) {
+						// 防止刷新 refresh-token 继续接收到的 401 错误，出现死循环
+						if (["/refresh-token"].some(url => request.url.endsWith(url))) {
 							window.location.href = `${import.meta.env.BASE_URL}login${rememberRoute()}`;
+							return response;
+						}
+
+						// If the token is expired, refresh it and try again.
+						const { refreshToken } = useUserStore.getState();
+						// If there is no refresh token, it means that the user has not logged in.
+						if (!refreshToken) {
+							window.location.href = `${import.meta.env.BASE_URL}login${rememberRoute()}`;
+							return response;
+						}
+
+						const freshResponse = await fetchRefreshToken({
+							refreshToken,
 						});
+
+						useUserStore.setState({
+							token: freshResponse.result.token,
+							refreshToken: freshResponse.result.refreshToken,
+						});
+
+						// Try again with the new token(options.hooks)
+						return ky(request, options);
 					}
 					else {
 						try {
@@ -49,10 +67,12 @@ const defaultConfig: Options = {
 							else {
 								message.error(response.statusText);
 							}
+							return response;
 						}
 						catch (e) {
 							console.error("Error parsing JSON:", e);
 							message.error(response.statusText);
+							return response;
 						}
 					}
 				}
