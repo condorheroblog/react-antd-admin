3b8b9e64647b544a54d84a3ff377878094ded146
Condor Hero <love2xinwei@gmail.com>
2025-07-21 13:42:38 +0800

    fix: convert Hook styles to object to address JSS warning

--- src/layout/layout-menu/style.ts
+++ src/layout/layout-menu/style.ts
@@ -1,11 +1,9 @@
 import { createUseStyles } from "react-jss";
 
-export const useStyles = createUseStyles(() => {
-	return {
-		menuBackgroundColor: {
-			"& .ant-menu-submenu-selected .ant-menu-submenu-title": {
-				backgroundColor: "var(--ant-menu-item-selected-bg)",
-			},
+export const useStyles = createUseStyles({
+	menuBackgroundColor: {
+		"& .ant-menu-submenu-selected .ant-menu-submenu-title": {
+			backgroundColor: "var(--ant-menu-item-selected-bg)",
 		},
-	};
+	},
 });
